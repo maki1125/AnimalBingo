cbb112db0a98a6e59fdca2cce8d924a33cef5449
工藤　真紀 <maki@kudoumakinoMacBook-Air.local>
2024-04-29 20:15:05 +0900

    フラッシュメッセージの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)#（postメソッドでUser.newからのデータが送られてきている）新規登録で作ったデータに(user_params)という引数をつけて@userに代入する  
     if @user.save#訳：@userが保存できたら
       login(user_params[:email], user_params[:password])#ユーザー登録したらログインする
-      redirect_to root_path #（ログイン出来たら「user_sessionsのnew」に飛ぶ(redirect_to)ようになる。*「user_sessionsについては後に説明）
+      redirect_to root_path, notice: "ユーザー登録しました。ログインしました。"#（ログイン出来たら「user_sessionsのnew」に飛ぶ(redirect_to)ようになる。*「user_sessionsについては後に説明）
     else#訳：@userが保存できなかったら
+      flash.now[:notice] = "ユーザー登録できませんでした。"
       render :new #（ログイン出来なければ、usersのnewに戻る（render）） 
     end
   end
