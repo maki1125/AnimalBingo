0fc4ba0f619da7359028a4de518e503c5e72db81
工藤　真紀 <maki@kudoumakinoMacBook-Air.local>
2024-05-06 16:10:17 +0900

    heroku更新

--- app/javascript/quiz_save.js
+++ app/javascript/quiz_save.js
@@ -9,7 +9,7 @@ let addElement = document.querySelector('.add');
 let addElement2 = document.querySelector('.add2');
 let addElement3 = document.querySelector('.add3');
 let cardElement = document.querySelector('.card');
-
+let outer = document.querySelector('.outer');
 addElement3.textContent = `${parseInt(friend_num)+30}` //仲間の数
 
 if (ok_num>0){
@@ -34,16 +34,37 @@ imgShow();
 
 // 各画像URLに対して処理を繰り返す
 function imgShow(){
-imgass.forEach(imagePath => {
+//imgass.forEach(imagePath => {
+for(let i = 0; i < 5; i++){
   //要素作成
   let divSquare = document.createElement('div');//セルのdiv要素作成。
   let div = document.createElement('div');//セル内に画像を配置するためのdiv要素作成。
   let img = document.createElement('img');;//img要素の作成。
-  cardElement.appendChild(divSquare);
-  divSquare.appendChild(img);//div要素の中にimg要素を追加。
+  if (i<ok_num){
+    img.src = imgass[i]; // 画像のURLを設定
+  }else{
+    img.src = question_imgass;
+  }
+  //col.appendChild(divSquare);
+  div.appendChild(img);//div要素の中にimg要素を追加。
+  divSquare.appendChild(div);
+  outer.appendChild(divSquare);
+  
   divSquare.classList.add('square'); //作成したセルにsquareクラスを追加。
-  // 画像のURLを設定
-  img.src = imagePath;
-});
-cardElement.style.width = `${new_num * 100+10}px`; // 100pxはsquare要素の幅に合わせた適切な幅を設定
+  //col.classList.add('col-md-3'); //作成したセルにsquareクラスを追加。
+  //col.classList.add('col-sm-6');
+ 
 }
+//});
+console.log(window.getComputedStyle(cardElement).width/(5/ok_num));
+//cardElement.style.width = `${parseInt(window.getComputedStyle(cardElement).width)/(5/ok_num)}px`; // 100pxはsquare要素の幅に合わせた適切な幅を設定
+}
+// 画面サイズに応じて変数を設定
+let squareWidth;
+if (window.innerWidth > 500) { // 例えば、768pxより大きい場合
+  squareWidth = 100/ok_num;
+} else { // それ以外の場合
+  squareWidth = 33;
+}
+
+//$('.square').css('flex', `0 0 ${squareWidth}%`);
