78fa9170f897363af55a15f1967b9241c048bb4f
工藤　真紀 <maki@kudoumakinoMacBook-Air.local>
2024-05-24 23:28:42 +0900

    ゲストログイン

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,8 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new' 
   post 'login', to: 'user_sessions#create' 
   delete 'logout', to: 'user_sessions#destroy' 
-  
+  #ゲストログイン
+  post '/guest_login', to: 'user_sessions#guest_login'
   #googleログイン
   post "oauth/callback" => "oauths#callback"
   get "oauth/callback" => "oauths#callback" 
